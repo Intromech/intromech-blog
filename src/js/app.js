164717525799ef2f90598b746889ce3e7848e5b0
Clash09809 <clashroyale09809@gmail.com>
2020-05-08 02:51:07 +0530

    Finished styling shwo page

--- src/js/app.js
+++ src/js/app.js
@@ -1,4 +1,20 @@
-const pageName = document.querySelector('.page-name h1').innerHTML.toLocaleLowerCase();
+const pageName = document.querySelector('.page-name h1').innerHTML.toLowerCase();
 const pageImg = document.querySelector('.page-name-container');
+const pageTitle = document.querySelector('title').innerHTML.toLowerCase();
+const articleImages = document.querySelectorAll('.show-article p img');
+// document.querySelector('.show-article img').classList.add("text-center");
+// console.log(document.querySelector('.show-article img'));
 
-pageImg.style.background = `url("/img/${pageName}.jpg") center no-repeat`
+// Setting Background Image of each page
+if (pageTitle == 'show'){
+    const imgURL = document.querySelector('.show-img').getAttribute("src")
+    pageImg.style.background = `url("${imgURL}") center no-repeat`
+} else {
+    pageImg.style.background = `url("/img/${pageName}.jpg") center no-repeat`
+}
+
+articleImages.forEach(img => {
+    img.parentElement.classList.add("text-center");
+})
+
+
